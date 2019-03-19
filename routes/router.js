e36c9b5c02b24a5bfdaddce51c9e8834ca7c9c41
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-03-20 02:04:05 +0530

    fix: Routes to models

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,9 @@
 var express = require("express");
 
 const app = express();
-var Patient = require("patient");
-var Doctor = require("doctor");
-var Prescription = require("prescription")
+var Patient = require("../models/patient");
+var Doctor = require("../models/doctor");
+var Prescription = require("../models/prescription")
 
 const router = express.Router();
 
